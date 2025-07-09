08c91988ccb7d113698701275bc34e87967e9dee
amirholya01 <120662275+amirhosseinolya@users.noreply.github.com>
2025-07-09 10:35:13 +0330

    Swagger contoller

---
+++ src/main/java/dk/dev/app/controller/SwaggerController.java
@@ -0,0 +1,21 @@
+package dk.dev.app.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class SwaggerController {
+
+    @GetMapping("/api-ui/main")
+    public String apiUI(){
+        return "forward:/swagger-ui/index.html";
+    }
+
+    @RequestMapping("/api-ui/**")
+    public String proxySwaggerUI(HttpServletRequest request){
+        String path = request.getRequestURI().replaceFirst("/api-ui", "/swagger-ui");
+        return  "forward:" + path;
+    }
+}
