60ff439100581ce415a5e00d0634b762468d0f56
amirholya01 <120662275+amirhosseinolya@users.noreply.github.com>
2025-07-09 08:43:35 +0330

    first commit

---
+++ src/main/java/dk/dev/app/SpringAppRunner.java
@@ -0,0 +1,13 @@
+package dk.dev.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAppRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAppRunner.class, args);
+    }
+
+}
