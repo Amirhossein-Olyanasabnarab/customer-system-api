691578e0bbbaabbd3d4e5ff0e5626302b7458c79
amirholya01 <120662275+amirhosseinolya@users.noreply.github.com>
2025-07-09 08:57:31 +0330

    CustomerDao-Interface

---
+++ src/main/java/dk/dev/app/dao/CustomerDao.java
@@ -0,0 +1,15 @@
+package dk.dev.app.dao;
+
+import dk.dev.app.model.Customer;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CustomerDao {
+    Customer save(Customer customer);
+    void deleteById(Long id);
+    Optional<Customer> findById(Long id);
+    List<Customer> findAll();
+    boolean existsById(Long id);
+    List<Customer> findByNameIgnoreCase(String name);
+}
