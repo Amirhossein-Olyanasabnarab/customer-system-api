2060bcec7e22ac4a22c5c13645d3603d23445d02
amirholya01 <120662275+amirhosseinolya@users.noreply.github.com>
2025-07-12 10:44:13 +0330

    consoleAppRunner

---
+++ src/main/java/dk/dev/app/console/ConsoleAppRunner.java
@@ -0,0 +1,21 @@
+package dk.dev.app.console;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+@Component
+@Profile("console")
+public class ConsoleAppRunner implements CommandLineRunner {
+    private final ConsoleInterface console;
+    @Autowired
+    public ConsoleAppRunner(ConsoleInterface console) {
+        this.console = console;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        console.start();
+    }
+}
