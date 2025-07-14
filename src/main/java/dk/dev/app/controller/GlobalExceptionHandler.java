0f72c5a4faae2ab70ef54a251d0a2c0f433e65f7
amirholya01 <120662275+amirhosseinolya@users.noreply.github.com>
2025-07-14 14:44:07 +0330

    Creating GlobalExceptionHandler-CustomerNotFoundException

---
+++ src/main/java/dk/dev/app/controller/GlobalExceptionHandler.java
@@ -0,0 +1,18 @@
+package dk.dev.app.controller;
+
+import dk.dev.app.dto.ErrorResponse;
+import dk.dev.app.exception.CustomerNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(CustomerNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleCustomerNotFoundException(CustomerNotFoundException exception) {
+        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.NOT_FOUND.value(), exception.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+    }
+}
