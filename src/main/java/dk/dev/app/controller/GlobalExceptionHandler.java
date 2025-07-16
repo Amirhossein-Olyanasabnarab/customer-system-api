8a0a2440b23fa621dd694468cccaf5ffcf49e10c
amirholya01 <120662275+amirhosseinolya@users.noreply.github.com>
2025-07-16 13:01:46 +0330

    Using DuplicatedCustomerException as RestControllerAdvice

--- src/main/java/dk/dev/app/controller/GlobalExceptionHandler.java
+++ src/main/java/dk/dev/app/controller/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package dk.dev.app.controller;
 
 import dk.dev.app.dto.ErrorResponse;
 import dk.dev.app.exception.CustomerNotFoundException;
+import dk.dev.app.exception.DuplicatedCustomerException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -14,5 +15,12 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ErrorResponse> handleCustomerNotFoundException(CustomerNotFoundException exception) {
         ErrorResponse errorResponse = new ErrorResponse(HttpStatus.NOT_FOUND.value(), exception.getMessage());
         return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(DuplicatedCustomerException.class)
+    public ResponseEntity<ErrorResponse> handleDuplicatedCustomerException(DuplicatedCustomerException exception) {
+        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.CONFLICT.value(),
+                exception.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.CONFLICT);
     }
 }
